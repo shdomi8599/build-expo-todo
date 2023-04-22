c631a538c6d8870c14a77c024b69a2e1c36f9c82
shdomi8599 <shdomi@naver.com>
2023-04-23 00:41:32 +0900

    test : 알람 구현 시도

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { WebView } from "react-native-webview";
+// import * as Notifications from "expo-notifications";
 
 export default function App() {
+  //eas build -p android  >>>>.abb 명령어
+  //eas build -p android --profile preview >>> .apk 명령어
+
+  // React.useEffect(() => {
+  //   async function requestPermissionsAsync() {
+  //     const { granted } = await Notifications.requestPermissionsAsync();
+  //     if (!granted) {
+  //       alert("알림 권한이 필요합니다.");
+  //     }
+  //   }
+  //   requestPermissionsAsync();
+  // }, []);
+
+  // // 알림 등록
+  // function scheduleNotification() {
+  //   Notifications.scheduleNotificationAsync({
+  //     content: {
+  //       title: "알람",
+  //       body: "알람이당",
+  //     },
+  //     trigger: {
+  //       seconds: 10,
+  //     },
+  //   });
+  // }
+
+  // // 웹뷰 메시지 수신 이벤트 핸들러
+  // function handleWebViewMessage(event) {
+  //   const data = event.nativeEvent.data;
+  //   if (data === "scheduleNotification") {
+  //     scheduleNotification();
+  //   }
+  // }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{
+        uri: "https://shdomi8599.github.io/mobile-to-do-app/",
+      }}
+      // onMessage={handleWebViewMessage} // 웹뷰 메시지 수신 이벤트 핸들러 등록
+      // javaScriptEnabled={true} // 웹뷰에서 자바스크립트 실행을 허용
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
